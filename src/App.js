9759bbccdef21e466cd5f66392221742b44b6f08
sowmiyaramraj <sowmiyaramraj.0505@gmail.com>
2022-12-07 21:52:27 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import useState from "react";
+import Signin from "./register/signin";
+import Signup from "./register/signup";
+import Product from "./usersdata";
+
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import { Buffer } from 'buffer';
+global.Buffer = Buffer;
 
 function App() {
-  return (
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body">
+        <Router>
+           
+            <Routes>          
+            <Route path="/"  element={<Signup/>}/>
+            <Route path="/signin" element={<Signin/>}/>          
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/product" element={<Product/>}/>
+            </Routes>
+           </Router>
+        </div>
     </div>
   );
 }
